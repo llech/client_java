986d8c823b71731d2f681693b3770a0643768d9a
llech <l.lech@ringler.ch>
2021-01-05 10:02:54 +0100

    Add simpleclient_jakarta_servlet.
    Simpleclient implementing jakarta servlet-api (for Jetty 11 etc.)

---
+++ simpleclient_jakarta_servlet/src/main/java/io/prometheus/client/exporter/MetricsServlet.java
@@ -0,0 +1,71 @@
+package io.prometheus.client.exporter;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import io.prometheus.client.CollectorRegistry;
+import io.prometheus.client.exporter.common.TextFormat;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * The MetricsServlet class exists to provide a simple way of exposing the metrics values.
+ *
+ */
+public class MetricsServlet extends HttpServlet {
+
+  private CollectorRegistry registry;
+
+  /**
+   * Construct a MetricsServlet for the default registry.
+   */
+  public MetricsServlet() {
+    this(CollectorRegistry.defaultRegistry);
+  }
+
+  /**
+   * Construct a MetricsServlet for the given registry.
+   * @param registry collector registry
+   */
+  public MetricsServlet(CollectorRegistry registry) {
+    this.registry = registry;
+  }
+
+  @Override
+  protected void doGet(final HttpServletRequest req, final HttpServletResponse resp)
+          throws ServletException, IOException {
+    resp.setStatus(HttpServletResponse.SC_OK);
+    resp.setContentType(TextFormat.CONTENT_TYPE_004);
+
+    Writer writer = new BufferedWriter(resp.getWriter());
+    try {
+      TextFormat.write004(writer, registry.filteredMetricFamilySamples(parse(req)));
+      writer.flush();
+    } finally {
+      writer.close();
+    }
+  }
+
+  private Set<String> parse(HttpServletRequest req) {
+    String[] includedParam = req.getParameterValues("name[]");
+    if (includedParam == null) {
+      return Collections.emptySet();
+    } else {
+      return new HashSet<String>(Arrays.asList(includedParam));
+    }
+  }
+
+  @Override
+  protected void doPost(final HttpServletRequest req, final HttpServletResponse resp)
+          throws ServletException, IOException {
+    doGet(req, resp);
+  }
+
+}
---
+++ simpleclient_jakarta_servlet/src/test/java/io/prometheus/client/exporter/MetricsServletTest.java
@@ -0,0 +1,78 @@
+package io.prometheus.client.exporter;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import org.junit.Test;
+
+import io.prometheus.client.CollectorRegistry;
+import io.prometheus.client.Gauge;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class MetricsServletTest {
+
+  @Test
+  public void testWriterFiltersBasedOnParameter() throws IOException, ServletException {
+    CollectorRegistry registry = new CollectorRegistry();
+    Gauge.build("a", "a help").register(registry);
+    Gauge.build("b", "a help").register(registry);
+    Gauge.build("c", "a help").register(registry);
+
+    HttpServletRequest req = mock(HttpServletRequest.class);
+    when(req.getParameterValues("name[]")).thenReturn(new String[]{"a", "b", "oneTheDoesntExist", ""});
+    HttpServletResponse resp = mock(HttpServletResponse.class);
+    StringWriter stringWriter = new StringWriter();
+    PrintWriter writer = new PrintWriter(stringWriter);
+    when(resp.getWriter()).thenReturn(writer);
+
+    new MetricsServlet(registry).doGet(req, resp);
+
+    assertThat(stringWriter.toString()).contains("a 0.0");
+    assertThat(stringWriter.toString()).contains("b 0.0");
+    assertThat(stringWriter.toString()).doesNotContain("c 0.0");
+  }
+
+  @Test
+  public void testWriterIsClosedNormally() throws IOException, ServletException {
+    HttpServletRequest req = mock(HttpServletRequest.class);
+    HttpServletResponse resp = mock(HttpServletResponse.class);
+    PrintWriter writer = mock(PrintWriter.class);
+    when(resp.getWriter()).thenReturn(writer);
+    CollectorRegistry registry = new CollectorRegistry();
+    Gauge a = Gauge.build("a", "a help").register(registry);
+
+    new MetricsServlet(registry).doGet(req, resp);
+    verify(writer).close();
+  }
+
+  @Test
+  public void testWriterIsClosedOnException() throws IOException, ServletException {
+    HttpServletRequest req = mock(HttpServletRequest.class);
+    HttpServletResponse resp = mock(HttpServletResponse.class);
+    PrintWriter writer = mock(PrintWriter.class);
+    when(resp.getWriter()).thenReturn(writer);
+    doThrow(new RuntimeException()).when(writer).write(any(char[].class), anyInt(), anyInt());
+    CollectorRegistry registry = new CollectorRegistry();
+    Gauge a = Gauge.build("a", "a help").register(registry);
+
+    try {
+      new MetricsServlet(registry).doGet(req, resp);
+      fail("Exception expected");
+    } catch (Exception e) {
+    }
+
+    verify(writer).close();
+  }
+}
